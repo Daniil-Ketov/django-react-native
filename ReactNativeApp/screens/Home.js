cee9fb9bc9f15a2cef60b6c0dd7790d3773c6cba
Ketov Daniil <ketov.daniil123@yandex.ru>
2023-03-05 16:28:31 +0300

    Changed to View with FlatList and FAB

--- ReactNativeApp/screens/Home.js
+++ ReactNativeApp/screens/Home.js
@@ -1,16 +1,40 @@
 import React, {useState} from 'react'
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { Card } from 'react-native-paper';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { Card, FAB } from 'react-native-paper';
 
-function Home(props) {
-    const [name, setName] = useState("DefaultName")
+const mydata = [
+    {id:1, title:"First title", description:"First description"},
+    {id:2, title:"Second title", description:"Second description"},
+    {id:2, title:"Third title", description:"Third description"},
+]
+
+const renderData = (item) => {
     return (
         <Card style={styles.cardStyle}>
-        <Text style={{fontSize:25}}>
-            {name}
-        </Text>
-        <Button title="Change text" onPress={()=>setName("Changed name")}/>
+            <Text style={{fontSize:25}}>{item.title}</Text>
+            <Text style={{fontSize:20}}>{item.description}</Text>
         </Card>
+    )
+}
+
+function Home(props) {
+    return (
+        <View>
+        <FlatList
+            data = {mydata}
+            renderItem = {({item}) => {
+                return renderData(item)
+            }}
+            keyExtractor = {item => item.id}
+        />
+        <FAB
+            style = {styles.fab}
+            small = {false}
+            icon = "plus"
+            theme = {{colors:{accent:"blue"}}}
+            onPress = {() => console.log("pressed")}
+        />
+        </View>
   )
 }
 
@@ -18,6 +42,14 @@ const styles = StyleSheet.create({
     cardStyle: {
         padding:10,
         margin:10,
+    },
+
+    fab: {
+        position: 'absolute',
+        margin: 16,
+        right: 0,
+        bottom: 0,
+        backgroundColor: 'blue',
     }
 })
 
